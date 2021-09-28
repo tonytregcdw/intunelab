3b8a78eca6df509e0e5cb75159e51d81d4a407c7
tonytregcdw <54070632+tonytregcdw@users.noreply.github.com>
2021-09-28 11:09:03 +0100

    Update azuredeploy.tf

--- azuredeploy.tf
+++ azuredeploy.tf
@@ -62,11 +62,6 @@ resource "azurerm_virtual_network_peering" "peer2" {
   allow_forwarded_traffic      = true
 }
 
-resource "azurerm_virtual_desktop_workspace_application_group_association" "workspacedesktopapp" {
-  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
-  application_group_id = azurerm_virtual_desktop_application_group.desktopapp.id
-}
-
 #RDP Access Rules for Lab
 #Get Client IP Address for NSG
 data "http" "clientip" {
