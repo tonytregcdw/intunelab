355f5b50e805f66c0e3d9b5dfb045759257d32ca
tonytregcdw <54070632+tonytregcdw@users.noreply.github.com>
2021-09-28 11:12:28 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,7 +13,7 @@ resource "local_file" "AnsibleInventory" {
 resource "local_file" "AzureResources" {
  content = templatefile("group_vars/azresources.tmpl",
  {
-  var_azurerg = azurerm_resource_group.wvdrg.name
+  var_azurerg = azurerm_resource_group.w10endpointrg.name
   var_vmname = azurerm_windows_virtual_machine.r1-win1001-vm.name
   var_vmpip = azurerm_public_ip.r1-win1001-pip.ip_address
  }
